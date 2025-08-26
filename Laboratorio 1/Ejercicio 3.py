a3f1080556a10d143ff61d45f08686054312c2f2
Cristian Lopez <53580059+Cristianlopjosue@users.noreply.github.com>
2025-08-25 23:15:16 -0400

    Ejercicio 3.py

--- Laboratorio 1/Ejercicio 3.py
+++ Laboratorio 1/Ejercicio 3.py
@@ -1,24 +1,25 @@
 import math
 class Problema3:
     def __init__(self):
-        self.__numeros=[0]*10
-        for i in range(0,10):
-            self.__numeros[i]=float(input())
+        print("Ingrese los datos del 1 al 10 =")
+        self.__d1=float(input())
+        self.__d2=float(input())
+        self.__d3=float(input())
+        self.__d4=float(input())
+        self.__d5=float(input())
+        self.__d6=float(input())
+        self.__d7=float(input())
+        self.__d8=float(input())
+        self.__d9=float(input())
+        self.__d10=float(input())
     def promedio(self):
-        T=0
-        for i in range (0,10):
-            aux=self.__numeros[i]
-            T=T+aux
-        T=T/10
-        self.__t=T
-        return T
+        self.__P=self.__d1+self.__d2+self.__d3+self.__d4+self.__d5+self.__d6+self.__d7+self.__d8+self.__d9+self.__d10
+        self.__t=self.__P/10
+        return self.__t
     def desviacion(self):
-        D=0
-        for i in range (0,10):
-            aux2=(math.pow((self.__numeros[i]-self.__t),2))
-            D=D+aux2
-        D=math.sqrt(D/9)
-        return D
+        self.__C=((self.__d1-self.__t)**2+(self.__d2-self.__t)**2+(self.__d3-self.__t)**2+(self.__d4-self.__t)**2+(self.__d5-self.__t)**2+(self.__d6-self.__t)**2+(self.__d7-self.__t)**2+(self.__d8-self.__t)**2+(self.__d9-self.__t)**2+(self.__d10-self.__t)**2)
+        self.__D=math.sqrt(self.__C/9)
+        return self.__D
 print("Ingrese los numeros =")
 ecuacion=Problema3()
 print("El promedio es =",ecuacion.promedio())
